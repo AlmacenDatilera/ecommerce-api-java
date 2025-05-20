e83e10faaadae4bfba667bdeb8651525cf6814de
LeandroZacariaz <leandrozacariaz18@gmail.com>
2025-05-20 11:29:35 -0300

    agregando healthcontroller

---
+++ src/main/java/com/ecommerce/ecommerce/config/HealthController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "{\"status\": \"ok\"}";
+    }
+}
