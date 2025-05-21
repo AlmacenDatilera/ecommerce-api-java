32157d6561886839f475e917d076c2fb709808ac
LeandroZacariaz <leandrozacariaz18@gmail.com>
2025-05-21 10:05:40 -0300

    modificando SecurityConfig

--- src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:5173"
+            "http://localhost:5173, https://iconic-indumentaria.vercel.app/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
